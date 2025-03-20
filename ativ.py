4d117265c14011331231a512d5bfb356282d25dc
Marcelo Olivetti <marceloolivetti@usp.br>
2025-03-20 11:02:57 -0300

    Frases

--- ativ.py
+++ ativ.py
@@ -1,7 +1,20 @@
 import random 
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+	"Frase A",
+	"Frase B",
+	"Frase C"
+	]
+parte2 = [
+	"Frase D",
+	"Frase E",
+	"Frase F"
+ ]
+parte3 = [
+	"Frase H",
+	"Frase I",
+	"Frase J"
+
+]
 
 print(random.choice(parte1),random.choice(parte2),random.choice(parte3))
